4af0008883bd35ad9376a13de7316308b872ac0f
Nguyen-Thanh-Tri <2212thanhttricat@gmail.com>
2024-09-05 13:03:39 +0700

    hehe

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: media,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["checked"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
